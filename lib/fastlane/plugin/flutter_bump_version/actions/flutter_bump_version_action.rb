05a1bb5a02759c7fc2dfc67878131247dd224c59
Mohammed chahboun <m97.chahboun@gmail.com>
2022-09-22 20:48:44 +0100

    Refactor bump_version method for fix test failed

--- lib/fastlane/plugin/flutter_bump_version/actions/flutter_bump_version_action.rb
+++ lib/fastlane/plugin/flutter_bump_version/actions/flutter_bump_version_action.rb
@@ -48,35 +48,51 @@ module Fastlane
         @pubspec_file_writter = FileWritter.new(pubspec_path)
       end
 
+      def congigure_major
+        return @is_major ? @current_version_in_hash['major'] + 1 : @current_version_in_hash['major']
+      end
+
+      def configure_minor
+        if @is_minor
+          @current_version_in_hash['minor'] + 1
+        else
+          @is_major ? 0 : @current_version_in_hash['minor']
+        end
+      end
+
+      def configure_patch
+        if @is_patch
+          @current_version_in_hash['patch'] + 1
+        else
+          @is_major || @is_minor ? 0 : @current_version_in_hash['patch']
+        end
+      end
+
+      def configure_build
+        if @is_build
+          @current_version_in_hash['build'] + 1
+        else
+          @is_major || @is_minor || @is_patch ? 0 : @current_version_in_hash['minor']
+        end
+      end
+
       def bump_version(part)
-        is_major = part == "major"
-        is_minor = part == "minor"
-        is_patch = part == "patch"
-        is_build = part == "build"
+        @is_major = part == "major"
+        @is_minor = part == "minor"
+        @is_patch = part == "patch"
+        @is_build = part == "build"
         current_version = @pubspec_yaml_reader.field('version')
         split_current_version = current_version.split(".")
         build_exist = current_version.count("+") != 0
-        patch = build_exist ? split_current_version[2].split("+")[0] : split_current_version[2]
-        build = build_exist ? split_current_version[2].split("+")[1] : "0"
-        current_version_in_hash = Hash("major" => split_current_version[0].to_i, "minor" => split_current_version[1].to_i, "patch" => patch.to_i, "build" => build.to_i)
-        current_version_in_hash['major'] = is_major ? current_version_in_hash['major'] + 1 : current_version_in_hash['major']
-        current_version_in_hash['minor'] = if is_minor
-                                             current_version_in_hash['minor'] + 1
-                                           else
-                                             is_major ? 0 : current_version_in_hash['minor']
-                                           end
-        current_version_in_hash['patch'] = if is_patch
-                                             current_version_in_hash['patch'] + 1
-                                           else
-                                             is_major || is_minor ? 0 : current_version_in_hash['patch']
-                                           end
-        current_version_in_hash['build'] = if is_build
-                                             current_version_in_hash['build'] + 1
-                                           else
-                                             is_major || is_minor || is_patch ? 0 : current_version_in_hash['minor']
-                                           end
-        new_version = "#{current_version_in_hash['major']}.#{current_version_in_hash['minor']}.#{current_version_in_hash['patch']}"
-        new_version = is_build ? new_version + "+#{current_version_in_hash['build']}" : new_version
+        patch = build_exist ? split_current_version.last.split("+")[0] : split_current_version[2]
+        build = build_exist ? split_current_version.last.split("+")[1] : "0"
+        @current_version_in_hash = Hash("major" => split_current_version[0].to_i, "minor" => split_current_version[1].to_i, "patch" => patch.to_i, "build" => build.to_i)
+        @current_version_in_hash['major'] = congigure_major
+        @current_version_in_hash['minor'] = configure_minor
+        @current_version_in_hash['patch'] = configure_patch
+        @current_version_in_hash['build'] = configure_build
+        new_version = "#{@current_version_in_hash['major']}.#{@current_version_in_hash['minor']}.#{@current_version_in_hash['patch']}"
+        new_version = @is_build ? new_version + "+#{@current_version_in_hash['build']}" : new_version
         update_pubspec(new_version, current_version)
       end
 
